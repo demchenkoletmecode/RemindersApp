d9841d60bfb44ada448c65f42b8698b030efd83d
Andrew <Demchenko@letmecode.org>
2022-11-07 11:13:45 +0300

    added a lightweight migration

--- RemindersApp/Business Logic/Database/CoreDataManager.swift
+++ RemindersApp/Business Logic/Database/CoreDataManager.swift
@@ -16,6 +16,12 @@ class CoreDataManager {
     
     private lazy var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "RemindersApp")
+        
+        let description = NSPersistentStoreDescription()
+        description.shouldMigrateStoreAutomatically = true
+        description.shouldInferMappingModelAutomatically = true
+        container.persistentStoreDescriptions =  [description]
+        
         container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
